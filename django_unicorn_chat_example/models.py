76c13a81825198a4dad8b330622621943f234218
Tim <tim@cyface.com>
2023-10-17 15:51:17 -0600

    Initial

--- django_unicorn_chat_example/models.py
+++ django_unicorn_chat_example/models.py
@@ -1,4 +1,4 @@
-from django.db.models import Model, ForeignKey, CharField, SET_NULL
+from django.db.models import Model, ForeignKey, CharField, DO_NOTHING
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 class Channel(Model):
     name = CharField(max_length=100)
     description = CharField(max_length=1000)
-    created_by_user = ForeignKey(User, on_delete=SET_NULL, null=True)
+    created_by_user = ForeignKey(User, on_delete=DO_NOTHING, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -15,8 +15,8 @@ class Channel(Model):
 
 
 class Message(Model):
-    from_user = ForeignKey(User, on_delete=SET_NULL, null=True)
-    channel = ForeignKey(Channel, on_delete=SET_NULL, null=True)
+    from_user = ForeignKey(User, on_delete=DO_NOTHING, null=True)
+    channel = ForeignKey(Channel, on_delete=DO_NOTHING, null=True)
     content = CharField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
